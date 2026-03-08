e2d011f2fa616e52f2720d452615552be4f1379f
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-09 00:19:44 +0530

    add assests

---
+++ frontend/src/assets/phots.js
@@ -0,0 +1,12 @@
+let photos = [
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119443/protodash_goty8r.jpg", // protodash
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119443/codeforge_tsg9b9.jpg", // codeforge
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119443/quiz1_maf1yl.jpg", // codefest
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119442/cpp-inter_ukga6r.jpg", // cpp inter
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119442/cpp_jjks08.jpg", // INTRO CPP
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119442/c_fqe8fu.jpg", // intro c
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1772094045/fe836574-40eb-4907-83e5-3555b5960c47_tpejk1.png", // web intro 
+
+]
+
+export default photos 
