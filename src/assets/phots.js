34c7134191615053d08a18ccb113a218d32b6dcc
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-26 22:35:13 +0530

    add photo links and other details

--- src/assets/phots.js
+++ src/assets/phots.js
@@ -6,7 +6,8 @@ let photos = [
     "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119442/cpp_jjks08.jpg", // INTRO CPP
     "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775119442/c_fqe8fu.jpg", // intro c
     "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1772094045/fe836574-40eb-4907-83e5-3555b5960c47_tpejk1.png", // web intro 
-
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775499823/14c5fd0f-868f-4dd6-9b60-4655957b1aa5_rvvbtv.png", // js inter
+    "https://res.cloudinary.com/dzeyeqk8e/image/upload/v1775499823/14c5fd0f-868f-4dd6-9b60-4655957b1aa5_rvvbtv.png", // js intro
 ]
 
 export default photos 
